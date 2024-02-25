8f98b2989801ad8ecccda7fbce7bfc77c1a6b173
grecpt <grecpt@gmail.com>
2024-02-25 22:12:34 +1000

    Add the home and about page and views

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from movie import views as MovieViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", MovieViews.home),
+    path("about/", MovieViews.about),
 ]
