9ea64fc00821809ae73a487c768ddca9d693fc2e
grecpt <grecpt@gmail.com>
2024-02-25 22:58:55 +1000

    Update the views to render the home template

--- movie/views.py
+++ movie/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Welcome to Home Page</h1>')
+    return render(request, "home.html")
 
 
 def about(request):
